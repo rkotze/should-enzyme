1d247dc1c348d064c7b2db215226bd36f97d2e41
Richard Kotze <rkotze@dctfh.com>
2016-09-24 16:58:22 +0000

    use should-sinon and check wrapper name is called.

--- src/assertions/assertion-builder.js
+++ src/assertions/assertion-builder.js
@@ -1,10 +1,11 @@
 import should from 'should';
-// import WrapperBuilder from './wrapper';
 
 const Assertion = should.Assertion;
 
-export function boolAssertBuilder(name) {
+export function boolAssertBuilder(name, wrapperBuilder) {
   Assertion.add(name, function() {
+    const wrapper = wrapperBuilder(this.obj);
 
+    should(wrapper[name](arguments[0])).be.true(' ');
   });
 }
--- src/assertions/assertion-builder.spec.js
+++ src/assertions/assertion-builder.spec.js
@@ -1,18 +1,49 @@
 import should from 'should';
-import { boolAssertBuilder } from './assertion-builder';
 import sinon from 'sinon';
+import 'should-sinon';
+import { shallow } from 'enzyme';
+import React from 'react';
+import { boolAssertBuilder } from './assertion-builder';
 
-describe('Get assert', () => {
-  let assertionAddSpy;
+const Fixture = () => (
+  <div>Content here</div>
+);
+
+describe('Assertion builder', () => {
+  let assertionAddSpy,
+  wrapperBuilderSpy,
+  renderDom,
+  wrapperProps;
 
   before(() => {
+    wrapperProps = { 
+      awesome: sinon.stub().returns(true) 
+    };
     assertionAddSpy = sinon.spy(should.Assertion, 'add');
+    wrapperBuilderSpy = sinon.stub().returns(wrapperProps);
+    
+    boolAssertBuilder('awesome', wrapperBuilderSpy);
+    renderDom = shallow(<Fixture />);
+    renderDom.should.be.awesome('stuff');
+  });
+
+  after(() => {
+    assertionAddSpy.restore();
   });
 
   it('call Assertion.add with "awesome"', () => {
-    boolAssertBuilder('awesome');
     assertionAddSpy.calledOnce.should.be.true();
     assertionAddSpy.calledWith('awesome', sinon.match.func);
+  });
+
+  it('call WrapperBuilder with an object', () => {
+    wrapperBuilderSpy.calledOnce.should.be.true();
+    wrapperBuilderSpy.calledWith(sinon.match.object).should.be.true();
+  });
+
+  it('should call the awesome method on wrapper', () => {
+    wrapperProps.awesome.should.be.calledOnce();
+    wrapperProps.awesome.should.be.calledWith(sinon.match.string);
   });
 
 });
