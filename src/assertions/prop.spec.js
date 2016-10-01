c44470e9b51802266d4470a24202029b9886e6d3
Richard Kotze <rkotze@dctfh.com>
2016-10-01 22:46:42 +0100

    refactor prop spec to check mount and shallow.

--- src/assertions/prop.spec.js
+++ src/assertions/prop.spec.js
@@ -13,61 +13,49 @@ PropFixture.propTypes = {
 
 describe('Should enzyme add prop', () => {
   const shouldEnzyme = should;
+  let wrapper;
 
   it('should have new method prop', () => {
     shouldEnzyme.should.have.property('prop');
   });
 
-  context('using ShallowWrapper', () => {
-    let wrapper;
+  const renderName = ['shallow', 'mount'];
+  [shallow, mount].forEach((renderMethod, i) => {
+    context(renderName[i], () => {
+      before(() => {
+        wrapper = renderMethod(<PropFixture id="content" />);
+      });
 
-    before(() => {
-      wrapper = shallow(<PropFixture id="content" />);
-    });
+      it('should have prop "id" in PropFixture', () => {
+        wrapper.should.have.prop('id');
+      });
 
-    it('should have prop "id" in PropFixture', () => {
-      wrapper.should.have.prop('id');
-    });
+      it('should have prop "id" with value "content" in PropFixture', () => {
+        wrapper.should.have.prop('id', 'content');
+      });
 
-    it('should have prop "id" with value "content" in PropFixture', () => {
-      wrapper.should.have.prop('id', 'content');
-    });
+      it('should NOT have prop "pizza" in PropFixture', () => {
+        wrapper.should.not.have.prop('pizza');
+      });
 
-    it('should NOT have prop "pizza" in PropFixture', () => {
-      wrapper.should.not.have.prop('pizza');
-    });
+      it('should have prop "id" but NOT with value "stuff" in PropFixture', () => {
+        wrapper.should.not.have.prop('id', 'stuff');
+      });
 
-    it('should have prop "id" but NOT with value "stuff" in PropFixture', () => {
-      wrapper.should.not.have.prop('id', 'stuff');
-    });
+      it('should fail to see useful error message for missing pizza prop', () => {
+        (() => wrapper.should.have.prop('pizza'))
+        .should.throwError(/expected '(div|PropFixture)' to have prop 'pizza'/);
+      });
 
-    it('should fail to see useful error message for missing pizza prop', () => {
-      (() => wrapper.should.have.prop('pizza'))
-      .should.throwError(/expected 'div' to have prop 'pizza'/);
-    });
+      it('should fail to see useful error message for incorrect expected prop value', () => {
+        (() => wrapper.should.have.prop('id', 'stuff'))
+        .should.throwError(/expected '(div|PropFixture)' prop 'id' to have value 'stuff', instead found 'content'/);
+      });
 
-    it('should fail to see useful error message for incorrect expected prop value', () => {
-      (() => wrapper.should.have.prop('id', 'stuff'))
-      .should.throwError(/expected 'div' prop 'id' to have value 'stuff', instead found 'content'/);
-    });
-
-    it('should fail to see useful error message for incorrect prop', () => {
-      (() => wrapper.should.have.prop('pizza', 'stuff'))
-      .should.throwError(/expected 'div' prop 'pizza' to have value 'stuff', instead found 'undefined'/);
+      it('should fail to see useful error message for incorrect prop', () => {
+        (() => wrapper.should.have.prop('pizza', 'stuff'))
+        .should.throwError(/expected '(div|PropFixture)' prop 'pizza' to have value 'stuff', instead found 'undefined'/);
+      });
     });
   });
-
-  context('using ReactWrapper', () => {
-    let wrapper;
-
-    before(() => {
-      wrapper = mount(<PropFixture someThing="content" />);
-    });
-
-    it('should have prop "someThing" in PropFixture', () => {
-      wrapper.should.have.prop('someThing');
-    });
-
-  });
-
 });
