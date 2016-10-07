4f8af68dbb291fa9b69f78c5ed1d30fb30a437f0
Richard Kotze <rkotze@dctfh.com>
2016-10-07 10:46:52 +0100

    Check if checkbox is checked.

---
+++ src/checked.spec.js
@@ -0,0 +1,30 @@
+import WrapperBuilder from './wrapper';
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+
+const CheckedFixture = () => (
+  <input type="checkbox" defaultChecked value="coffee" />
+);
+
+// const NotCheckedFixture = () => (
+//   <input type="checkbox" value="coffee" />
+// );
+
+describe('Checkbox', () => {
+  const methodNames = ['shallow', 'mount'];
+  [shallow, mount].forEach((renderMethod, i) => {
+    let checkedInput;
+     // notCheckedInput;
+    before(() => {
+      checkedInput = WrapperBuilder(renderMethod(<CheckedFixture />));
+      // notCheckedInput = WrapperBuilder(renderMethod(<NotCheckedFixture />));
+    });
+
+    context(methodNames[i], () => {
+      it(`should be checked`, () => {
+        checkedInput.checked().should.be.true();
+      });
+    });
+
+  });
+});
