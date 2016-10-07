0089918d2b03b5fb6196861448a393127a92c554
Richard Kotze <rkotze@dctfh.com>
2016-10-07 10:48:50 +0100

    Check if checkbox is not checked.

--- src/checked.spec.js
+++ src/checked.spec.js
@@ -6,23 +6,26 @@ const CheckedFixture = () => (
   <input type="checkbox" defaultChecked value="coffee" />
 );
 
-// const NotCheckedFixture = () => (
-//   <input type="checkbox" value="coffee" />
-// );
+const NotCheckedFixture = () => (
+  <input type="checkbox" value="coffee" />
+);
 
 describe('Checkbox', () => {
   const methodNames = ['shallow', 'mount'];
   [shallow, mount].forEach((renderMethod, i) => {
-    let checkedInput;
-     // notCheckedInput;
+    let checkedInput, notCheckedInput;
     before(() => {
       checkedInput = WrapperBuilder(renderMethod(<CheckedFixture />));
-      // notCheckedInput = WrapperBuilder(renderMethod(<NotCheckedFixture />));
+      notCheckedInput = WrapperBuilder(renderMethod(<NotCheckedFixture />));
     });
 
     context(methodNames[i], () => {
       it(`should be checked`, () => {
         checkedInput.checked().should.be.true();
+      });
+
+      it(`should NOT be checked`, () => {
+        notCheckedInput.checked().should.be.false();
       });
     });
 
