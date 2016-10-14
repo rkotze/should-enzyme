c6fb21d4e292671f1ea3a72f0495cd305dd16a23
Richard Kotze <rkotze@dctfh.com>
2016-10-14 23:20:41 +0100

    create the element prop.

--- src/render-enzyme.js
+++ src/render-enzyme.js
@@ -5,5 +5,17 @@ export default class RenderEnzyme extends BaseEnzyme {
     super();
     this.enzyme = enzymeWrapper;
   }
+
+  get element (){
+  	if(!this.__element) {
+  		if(this.enzyme.first()['0'].type === 'root') {
+  			this.__element = this.enzyme.children().first();
+  		} else {
+  			this.__element = this.enzyme.first();
+  		}
+  	}
+
+  	return this.__element;
+  }
   
 }
