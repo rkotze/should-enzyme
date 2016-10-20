14249a9298dc5aaa7cc617c3ddde64069119e022
Richard Kotze <rkotze@dctfh.com>
2016-10-20 22:12:40 +0000

    add exact text assert, implementation and tests.

--- src/assertions/text.spec.js
+++ src/assertions/text.spec.js
@@ -1,6 +1,3 @@
-/* eslint-env mocha */
-/* eslint-disable padded-blocks, no-unused-expressions, max-len, no-console */
-
 import './text';
 import { shallow, mount, render } from 'enzyme';
 import React from 'react';
@@ -10,11 +7,12 @@ const TextFixture = () => (
   <div className="special burger">Content here. More content</div>
 );
 
-describe('Should enzyme add check text', () => {
+describe('Should enzyme add containsText', () => {
   const shouldEnzyme = should;
-  it('should have new method text', () => {
+  it('should have new method containText', () => {
     shouldEnzyme.should.have.property('containsText');
   });
+
   [shallow, mount, render].forEach(method => {
     context(method.name, () => {
       before(() => {
@@ -32,6 +30,35 @@ describe('Should enzyme add check text', () => {
       it(`assert should fail to see useful error message`, () => {
         (() => wrapper.should.containsText('pizza'))
         .should.throwError(/expected '(div|TextFixture)' to contain text 'pizza' but found 'Content here. More content'/);
+      });
+
+    });
+  });
+});
+
+describe('Should enzyme add text', () => {
+  const shouldEnzyme = should;
+  it('should have new method text', () => {
+    shouldEnzyme.should.have.property('text');
+  });
+
+  [shallow, mount, render].forEach(method => {
+    context(method.name, () => {
+      before(() => {
+        wrapper = method(<TextFixture />);
+      });
+
+      it(`should contain text "Content here. More content" in TextFixture`, () => {
+        wrapper.should.text('Content here. More content');
+      });
+
+      it(`should NOT have text "pizza" in TextFixture`, () => {
+        wrapper.should.not.text('pizza');
+      });
+
+      it(`assert should fail when no exact text and show error message`, () => {
+        (() => wrapper.should.text('pizza'))
+        .should.throwError(/expected '(div|TextFixture)' to have exact text of 'pizza' but found 'Content here. More content'/);
       });
 
     });
