c0726ae5af3ae6ba701d9a70557c25fad3e2eb28
Richard Kotze <rkotze@dctfh.com>
2017-02-08 21:20:22 +0000

    fix error messages in state.

--- src/assertions/state.js
+++ src/assertions/state.js
@@ -11,13 +11,13 @@ Assertion.add('state', function(expectedKey, expectedValue){
   if(arguments.length > 1 && typeof wrapperState !== 'undefined') {
     this.params = {
       actual: wrapper.name(),
-      operator: `expected '${wrapper.name()}' state '${expectedKey}' property to have value '${expectedValue}', instead found '${wrapperState}'`
+      operator: `state '${expectedKey}' property to have value '${expectedValue}', instead found '${wrapperState}'`
     };
     should(wrapperState === expectedValue).be.true(' ');
   }else{
     this.params = {
       actual: wrapper.name(),
-      operator: `expected '${wrapper.name()}' to have state '${expectedKey}' property`
+      operator: `to have state '${expectedKey}' property`
     };
     should(assertKeyValue(wrapperState)).be.true(' ');
   }
