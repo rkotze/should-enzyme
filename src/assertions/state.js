84f5997d2a732a05dd33be963e7e9a27c590ca93
Richard Kotze <rkotze@dctfh.com>
2017-02-08 21:11:41 +0000

    removed commented code.

--- src/assertions/state.js
+++ src/assertions/state.js
@@ -1,19 +1,3 @@
-// import { assertionBuilder } from './assertion-builder';
-// import assertKeyValue from '../assert-key-value';
-
-// assertionBuilder(
-//   'state', 
-//   function (expectedKey, expectedValue) {
-//     return assertKeyValue(this.state(expectedKey), expectedValue);
-//   },
-//   function (expectedKey, expectedValue) {
-//     if(arguments.length === 2 && typeof this.state(expectedKey) !== 'undefined')
-//       return `expected '${this.name()}' state '${expectedKey}' property to have value '${expectedValue}', instead found '${this.state(expectedKey)}'`;
-
-//     return `expected '${this.name()}' to have state '${expectedKey}' property`;
-//   }
-// );
-
 import WrapperBuilder from '../wrapper';
 import assertKeyValue from '../assert-key-value';
 import should from 'should';
