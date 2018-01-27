39a9657e700c14b32d31136c4a2c344b82f088b5
Richard Kotze <richkotze@outlook.com>
2018-01-27 22:34:48 +0000

    Start to support Enzyme 3.x
    Upgrade dev dependences React 16, Cheerio 1 and Enzyme 3.
    Breaking change to `isPresent` for static `render` method. Now no longer present for null React components. Not sure why but added test to reflect this behaviour.
    
    Enzyme 3 introduces adaptors to create consistent API between versions of React. Could make things tricky, might have to do a similar thing in terms of knowing which version of Enzyme.

--- test-setup/each-render-method.js
+++ test-setup/each-render-method.js
@@ -1,10 +1,13 @@
-import { shallow, mount, render} from 'enzyme';
+import enzyme, { shallow, mount, render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+enzyme.configure({ adapter: new Adapter() });
 
 export function eachEnzymeMethod(methodNames, fn) {
   const methods = {
-    'shallow': shallow,
-    'mount': mount,
-    'render': render
+    shallow: shallow,
+    mount: mount,
+    render: render
   };
 
   methodNames.forEach((methodName, i) => {
