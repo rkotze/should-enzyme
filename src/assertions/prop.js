e2e486826454146b6d97b2dd11f4c25e88c03114
Richard Kotze <richkotze@outlook.com>
2018-05-07 15:15:30 +0100

    No args needed for undefined() assert
    Errors thrown because no args needed to be passed into the undefined() assert.
    
    Type checking has recently been added to should.js using TypeScript.

--- src/assertions/prop.js
+++ src/assertions/prop.js
@@ -3,21 +3,21 @@ import should from 'should';
 
 const Assertion = should.Assertion;
 
-Assertion.add('prop', function(expectedKey, expectedValue){
+Assertion.add('prop', function (expectedKey, expectedValue) {
   const wrapper = WrapperBuilder(this.obj),
-  wrapperProp = wrapper.prop(expectedKey);
+    wrapperProp = wrapper.prop(expectedKey);
 
-  if(arguments.length > 1 && typeof wrapperProp !== 'undefined') {
+  if (arguments.length > 1 && typeof wrapperProp !== 'undefined') {
     this.params = {
       actual: wrapper.name(),
       operator: `prop '${expectedKey}' to have value ${should.format(expectedValue)}, instead found ${should.format(wrapperProp)}`
     };
     should(wrapperProp).be.eql(expectedValue, ' ');
-  }else{
+  } else {
     this.params = {
       actual: wrapper.name(),
       operator: `to have prop '${expectedKey}'`
     };
-    should(wrapperProp).not.be.undefined(' ');
+    should(wrapperProp).not.be.undefined();
   }
 }); 
